0b7e0f8a699b26eeb763b98dda6314e404a71038
selimoz <y.selimoz@hotmail.com>
2021-06-24 02:08:13 +0100

    verify a kadar tamam

--- src/test/java/Test/vacuum_cleaner.java
+++ src/test/java/Test/vacuum_cleaner.java
@@ -27,26 +27,33 @@ public class vacuum_cleaner {
 
     @Test
     public void vacuum() throws InterruptedException {
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.amazon.co.uk");
         WebElement searchbox = driver.findElement(By.id("twotabsearchtextbox"));
         searchbox.sendKeys("vacuum cleaner");
        WebElement searchclick = driver.findElement(By.id("nav-search-submit-button"));
         searchclick.click();
-        Thread.sleep(4000);
+        Thread.sleep(2000);
         WebElement sort = driver.findElement ( By.xpath ( "//span[@class='a-button-text a-declarative']" ) );
         sort.click();
         Thread.sleep(4000);
         WebElement newest = driver.findElement(By.id("s-result-sort-select_4"));
         newest.click();
-        Thread.sleep(5000);
-        WebElement firstitem = driver.findElement(By.xpath("//div/h2/a/span[@class='a-size-medium a-color-base a-text-normal']"));
+        Thread.sleep(2000);
+        WebElement firstitem = driver.findElement(By.className("a-price-whole"));
         firstitem.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement quantity = driver.findElement(By.id("quantity"));
         quantity.click ();
-        WebElement qua2 = driver.findElement(By.cssSelector("select>option[value='2']"));
-
-
+        WebElement qua2 = driver.findElement(By.xpath("//div/span/select/option[@value='2']"));
+        qua2.click();
+        WebElement addtochart = driver.findElement(By.id("add-to-cart-button"));
+        addtochart.click();
+        Thread.sleep(2000);
+        WebElement nothanks = driver.findElement(By.xpath("(//span/input[@class='a-button-input'])[2]"));
+        nothanks.click();
+        Thread.sleep(2000);
+        WebElement price = driver.findElement(By.xpath("//div/div/span/span"));
+        price.getText().toString();
 
 
 
